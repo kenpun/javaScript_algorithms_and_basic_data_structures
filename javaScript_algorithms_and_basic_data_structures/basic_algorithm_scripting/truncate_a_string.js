fdd65a60d953aff5dbdd6525538431afdaa260fa
Kenneth Elumba <kennethelumba@gmail.com>
2022-11-21 15:59:46 +0100

    add solution to finders keepers

--- javaScript_algorithms_and_basic_data_structures/basic_algorithm_scripting/truncate_a_string.js
+++ javaScript_algorithms_and_basic_data_structures/basic_algorithm_scripting/truncate_a_string.js
@@ -14,3 +14,10 @@ function truncateString(str, num) {
   }
   
   truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+// We start off with a simple if statement to determine one of two outcomes…
+// If our string length is greater than the num we want to truncate it, we return a slice of 
+// our string starting at character 0, and ending at num. We then append our '...' to the end 
+// of the string.
+// However, if above situation is not true, it means our string length is less than our 
+// truncation num. Therefore, we can just return the string.
